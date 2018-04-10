8f158c459f32ac4b1ae9b971266b99ea759e18d1
julia sheremet <juliajessicasher@gmail.com>
2018-04-10 12:23:26 -0700

    merge changes from test branch to master

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 //backend logic
-function PizzaOrder(price =0, size, topping){ //constructor
+function PizzaOrder(name, price=0, size, toppings){ //constructor
+  this.name = name;
   this.price = price;
   this.size = size;
-  this.topping = topping;
+  this.toppings = toppings;
 }
 
 PizzaOrder.prototype.pizzaCalculation = function(){ //create a prototype method to calculate the price
@@ -16,10 +17,19 @@ PizzaOrder.prototype.pizzaCalculation = function(){ //create a prototype method 
     this.price += 10;
   } else if (this.size === "Extra Large") {
     this.price += 12;
-    console.log(this.price);
+    // console.log(this.price);
   }
-  return this.price;
-  console.log(this.price);
+
+  for (var i=0; i<=this.toppings.length;i++) {
+    if (this.toppings[i] === " Sausage" || this.toppings[i] === " Pepperoni") {
+    this.price += 2;
+    } else if (this.toppings[i] === " Mushrooms" || this.toppings[i] === " Green Peppers" || this.toppings[i] === " Olives") {
+    this.price += 1; //need to convert the parseint to decimals?
+    } else if (this.toppings[i] === " Extra Cheese") {
+    this.price += 1;
+    // console.log(this.toppings[i]);
+    }
+  }
 }
 
 function resetFields() {
@@ -34,47 +44,34 @@ $(document).ready(function(){
     e.preventDefault();
 
     var orderName = $("#order-name").val();
-    var price = 0;
     var size = $("#size").val();
     var toppingChoiceArray = [];
+    var price = 0;
 
-    $("input:checkbox[name=topping]:checked").each(function(){  //getting each selected topping
-      var topping = $(this).val();
-      toppingChoiceArray.push(" " + topping); //pushing selected toppings to the topping array
-      $('.topping-output').text(toppingChoiceArray);
-      for (var i=0; i<=toppingChoiceArray.length;i++) { //looping through array to find which topping items were selected
-        if (toppingChoiceArray === " Sausage" || toppingChoiceArray === " Pepperoni") {
-          var newPrice = price += 2;
-        } else if (toppingChoiceArray === " Mushrooms" || toppingChoiceArray === " Green Peppers" || toppingChoiceArray === " Olives") {
-          var newPrice = price += 1; //need to convert the parseint to decimals?
-        } else if (toppingChoiceArray === " Extra Cheese") {
-          var newPrice = price += 1;
-          console.log(newPrice);
-          console.log(toppingChoiceArray);
-        }
-      }
+    var checkToppings = $(".toppings:checked");
+    checkToppings.each(function(){
+      var toppingSeleted = $(this).val();
+      toppingChoiceArray.push(toppingSeleted);
+      console.log($(this));
     });
-    $("#order-output").append("<li><span class='pizzaOrderName'>" + orderName + ", click here for your order details" + "</span></li>"); //listing name for order details
+    var newPizzaOrder = new PizzaOrder(orderName, price, size, toppingChoiceArray); //instance that holds the values for each item
+    newPizzaOrder.pizzaCalculation();  //create a variable to run a method to calculate the price - running the backend logic
 
-    var newPizzaOrder = new PizzaOrder(price, size, toppingChoiceArray); //instance that holds the values for each item
+    $("#order-output").append("<li><span class='pizzaOrderName'>" + newPizzaOrder.name + ", click here for your order details" + "</span></li>"); //listing name for order details
 
-    var pizzaPriceEstimator = newPizzaOrder.pizzaCalculation();  //create a variable to run a method to calculate the price
-
-    console.log(newPizzaOrder);
-    console.log(pizzaPriceEstimator);
     $(".pizzaOrderName").last().click(function(){
-      var orderInformation = `<h2>Order Details:</h2>
-                              <img src='img/pizza.png'>
-                              <p class='lead outputDisplay'> ${orderName}, here are your order details:</p>
-                              <p class='lead outputDisplay'> Pizza Size: ${size}</p>
-                              <p class='lead outputDisplay'> Pizza Topping: ${toppingChoiceArray}</p>
-                              <p class='lead outputDisplay'> Price: $ ${pizzaPriceEstimator}</p>`;
+      var orderInformation = `<img src='img/pizza.png' alt='image of pizza'>
+                              <div class="flex output-styling" id="output-styling">
+                                <h3>Order Details:</h3>
+                                <hr>
+                                <p class='lead outputDisplay'> ${newPizzaOrder.name}, here's what you ordered:</p>
+                                <p class='lead outputDisplay'><span class="bold-output-text">Pizza Size:</span> ${newPizzaOrder.size}</p>
+                                <p class='lead outputDisplay'><span class="bold-output-text">Pizza Toppings:</span> ${newPizzaOrder.toppings}</p>
+                                <p class='lead outputDisplay'><span class="bold-output-text">Total Price:</span> $ ${newPizzaOrder.price}</p>
+                              </div>`;
 
       $(".show-order").html(orderInformation);
     });
     resetFields();
-  });
-  $("#reset").click(function(){
-
   });
 });
